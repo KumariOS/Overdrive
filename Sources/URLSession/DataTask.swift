46eac2858497a092c9ba0a4f81994380c5e0de3d
SaidSikira <saidsikira@gmail.com>
2016-12-11 13:46:52 +0100

    Remove JSONTask from URLSession target
    * Update DataTask documentation
    * Update URLSession tests

--- Sources/URLSession/DataTask.swift
+++ Sources/URLSession/DataTask.swift
@@ -9,31 +9,54 @@
 import Foundation
 import Overdrive
 
+/// Provides interface for loading requests
 open class DataTask: Task<(Data, URLResponse)> {
     
+    /// `URLRequest` object
     let request: URLRequest
+    
+    /// `URLSession` object
     weak var session: URLSession?
     
     var task: URLSessionDataTask?
     
     // MARK: Init methods
+    
+    /// Create new instance of `DataTask`
+    ///
+    /// - Parameters:
+    ///   - request: URL request
+    ///   - session: `URLSession` object, defaults to shared url session
     public init(request: URLRequest, session: URLSession = .shared) {
         self.request = request
         self.session = session
         
         super.init()
         
-        task = session.dataTask(with: request) { [weak self]
+        task = self.session?.dataTask(with: request) { [weak self]
             (result: Result<(Data, URLResponse)>) in
             self?.finish(with: result)
         }
     }
     
+    
+    /// Create `DataTask` instance with `URL` object
+    ///
+    /// - Parameters:
+    ///   - url: `URL`
+    ///   - session: `URLSession` object, defaults to shared url session
     convenience init(url: URL, session: URLSession = .shared) {
         let request = URLRequest(url: url)
         self.init(request: request, session: session)
     }
     
+    /// Create `DataTask` instance with url string
+    ///
+    /// - Parameters:
+    ///   - urlString: URL `String` object
+    ///   - session: `URLSession` object, defaults to shared url session
+    ///
+    /// - Returns: Optional `DataTask` instance (nil if url string is invalid)
     convenience init?(urlString: String, session: URLSession = .shared) {
         guard let url = URL(string: urlString) else {
             return nil
--- Tests/URLSessionTests/DataTaskTests.swift
+++ Tests/URLSessionTests/DataTaskTests.swift
@@ -7,10 +7,42 @@
 //
 
 import XCTest
+import Overdrive
 import TestSupport
 
 @testable import URLSession
 
 class DataTaskTests: XCTestCase {
     
+    enum DataTaskTestsError: Error {
+        case invalidData(Any)
+    }
+    
+    func testDataTask() {
+        let testExpectation = expectation(description: "DataTask expectation")
+        
+        let url = URL(string: "https://httpbin.org/get")!
+        let task = DataTask(url: url)
+        
+        task
+            .onValue { data, response in
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    guard let document = json as? [String: Any] else {
+                        throw DataTaskTestsError.invalidData(json)
+                    }
+                    
+                    let url = document["url"] as? String
+                    
+                    XCTAssertEqual(url, "https://httpbin.org/get")
+                    testExpectation.fulfill()
+                }
+            }.onError { error in
+                XCTFail(String(describing: error))
+        }
+        
+        TaskQueue.main.add(task: task)
+        
+        waitForExpectations(timeout: 1, handler: nil)
+    }
 }
